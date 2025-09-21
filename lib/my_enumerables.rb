44c3e229d5713bead1d5639ccc1e9589653ba17d
habidoyetech <abidoyeabiodun97@gmail.com>
2025-09-21 16:11:04 +0100

    create my_each_with_index method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,14 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    self.each do |elem|
+      yield(elem, index)
+      index += 1
+    end
+    self
+  end
+  
 end
 
 # You will first have to define my_each
@@ -8,4 +17,14 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    self.my_each do |elem|
+      yield elem 
+    end
+  end
+end
+
+[1,2,3,4,5].my_each_with_index do |elem, index|
+  p elem
+  p index
 end
